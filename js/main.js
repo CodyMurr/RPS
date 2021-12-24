d655302ea59e42b263e8a08d065279002a6d7f5f
CodyMurray-ga <codakmurraki@gmail.com>
2021-12-23 18:26:13 -0600

    Define state variables

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const start = {
+    user: {
+        wins: 0,
+        losses: 0,
+        choice: 'rock'
+    },
+    computer: {
+        wins: 0,
+        losses: 0, 
+        choice: 'rock'
+    },
+    ties: 0,
+    winner: 'null'
+};
+
+const choices = ['rock', 'paper', 'scissors'];
