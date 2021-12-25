83d3a276c9803e9b8a28cb2272e6a0f0ac5e0115
CodyMurray-ga <codakmurraki@gmail.com>
2021-12-24 18:46:35 -0600

    Update and restructure

--- js/main.js
+++ js/main.js
@@ -12,28 +12,29 @@ const choiceEls = [...document.querySelectorAll('#choices > button')];
 const message = document.getElementById('result');
 
 choiceEls.forEach(choice => {
-    
-    choice.addEventListener("click", init);
+    choice.addEventListener("click", function(e) {
+        const trgtIdx = choiceEls.indexOf(e.target);
+        user.choice = choices[trgtIdx];
+        let randIdx = Math.floor(Math.random() * choiceEls.length);
+        computer.choice = choices[randIdx];
+        render();
+    });
 });
 
-function init(e) {
-    const trgtIdx = choiceEls.indexOf(e.target);
-    user.choice = choices[trgtIdx];
-    
-    let randIdx = Math.floor(Math.random() * choiceEls.length - 1);
-    computer.choice = choices[randIdx];
-    
-    render();
-};
+
 
 function render() {
     if (user.choice === computer.choice) {
-        message.innerHTML = "Tie Game!";
-    } else if (((user.choice === 'rock') && (computer.choice === 'paper')) || ((user.choice === 'paper') && (computer.choice === 'scissors')) || ((user.choice === 'scissors') && (computer.choice === 'rock'))) {
-        winner = computer;
-        message.innerHTML = `${computer.choice.toUpperCase()} Beats ${user.choice.toUpperCase()}! COMPUTER Wins!`;
-    } else if (((user.choice === 'scissors') && (computer.choice === 'paper')) || ((user.choice === 'rock') && (computer.choice === 'scissors')) || ((user.choice === 'paper') && (computer.choice === 'rock'))) {
-        winner = user;
-        message.innerHTML = `${user.choice.toUpperCase()} Beats ${computer.choice.toUpperCase()}! USER Wins!`;
+            message.innerHTML = "Tie Game!";
+    } else if (((user.choice === 'rock') && (computer.choice === 'paper')) || 
+        ((user.choice === 'paper') && (computer.choice === 'scissors')) || 
+        ((user.choice === 'scissors') && (computer.choice === 'rock'))) {
+            winner = computer;
+            message.innerHTML = `${computer.choice.toUpperCase()} Beats ${user.choice.toUpperCase()}! COMPUTER Wins!`;
+    } else if (((user.choice === 'scissors') && (computer.choice === 'paper')) || 
+        ((user.choice === 'rock') && (computer.choice === 'scissors')) || 
+        ((user.choice === 'paper') && (computer.choice === 'rock'))) {
+            winner = user;
+            message.innerHTML = `${user.choice.toUpperCase()} Beats ${computer.choice.toUpperCase()}! USER Wins!`;
     } 
 };
