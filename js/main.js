2bcec70d06136ca3f8040c102ca4ef5f63990c20
CodyMurray-ga <codakmurraki@gmail.com>
2021-12-28 15:51:38 -0600

    Add scoreboard and re-purpose reset button

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const user = {
     name: "User",
-    choice: 'null'
+    choice: 'null',
+    score: 0
 };
 const computer = {
     name: "Computer",
-    choice: 'null'
+    choice: 'null',
+    score: 0
 };
 const images = {
     rock: {
@@ -23,11 +25,15 @@ const images = {
 
 let winner;
 let loser;
+let ties = 0;
+let reset = false;
 let choices = [images.rock, images.paper, images.scissors]
 
 const choiceEls = [...document.querySelectorAll('#choices > img')];
-const reset = document.querySelector('#reset-btn');
 const message = document.getElementById('result');
+const userScore = document.getElementById('user-score');
+const computerScore = document.getElementById('computer-score');
+const numTies = document.getElementById('ties');
 
 choiceEls.forEach(choice => {
     choice.addEventListener("click", function(e) {
@@ -35,42 +41,48 @@ choiceEls.forEach(choice => {
         user.choice = choices[trgtIdx];
         let randIdx = Math.floor(Math.random() * choiceEls.length);
         computer.choice = choices[randIdx];
-        if (winner) return;
         render();     
     });
 });
 
-reset.addEventListener("click", function() {
-    reset.hidden = true;
-    winner = null;
-    message.innerHTML = "";
-    document.querySelector('#user').style.borderColor = 'white';
-    document.querySelector('#computer').style.borderColor = 'white';
-});
+document.querySelector('#reset-btn').addEventListener("click", function() {
+    reset = true;
+    renderMessage();
+})
+    
 
 function render() {
+    reset = false;
     if (user.choice === computer.choice) {
-            message.innerHTML = "Tie Game!";
+        winner = null;
     } else if (((user.choice === choices[0]) && (computer.choice === choices[1])) || 
-        ((user.choice === choices[1]) && (computer.choice === choices[2])) || 
-        ((user.choice === choices[2]) && (computer.choice === choices[0]))) {
-            winner = computer;
-            loser = user;
+    ((user.choice === choices[1]) && (computer.choice === choices[2])) || 
+    ((user.choice === choices[2]) && (computer.choice === choices[0]))) {
+        winner = computer;
+        loser = user;
     } else if (((user.choice === choices[2]) && (computer.choice === choices[1])) || 
-        ((user.choice === choices[0]) && (computer.choice === choices[2])) || 
-        ((user.choice === choices[1]) && (computer.choice === choices[0]))) {
-            winner = user;
-            loser = computer;
+    ((user.choice === choices[0]) && (computer.choice === choices[2])) || 
+    ((user.choice === choices[1]) && (computer.choice === choices[0]))) {
+        winner = user;
+        loser = computer;
     } 
     renderMessage();
 };  
 
 function renderMessage() {
-    if (winner) {
+    if (reset) {
+        user.score = 0;
+        computer.score = 0;
+        ties = 0;
+        message.innerHTML = "";
+    } else if (winner) {
         message.innerHTML = `${winner.choice.alt.toUpperCase()} Beats ${loser.choice.alt.toUpperCase()}! ${winner.name} Wins!`;
-        document.querySelector(`#${winner.name.toLowerCase()}`).style.borderColor = 'gray';
-        reset.hidden = false;
-    };
-}    
-
-
+        winner.score += 1;
+    } else if (!winner) {
+        message.innerHTML = "Tie Game!";
+        ties += 1;
+    }
+    userScore.innerHTML = `${user.name}: ${user.score}`;
+    computerScore.innerHTML = `${computer.name}: ${computer.score}`;
+    numTies.innerHTML = `Ties: ${ties}`;
+};
